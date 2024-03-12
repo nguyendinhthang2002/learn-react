ebe64009c79d977e15c963651add0ec0f5b8d3f7
Owen-Walker <tn2k2tb@gmail.com>
2024-03-12 14:40:14 +0700

    Update name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by Thang
+          Learn React by russell
         </a>
       </header>
     </div>
